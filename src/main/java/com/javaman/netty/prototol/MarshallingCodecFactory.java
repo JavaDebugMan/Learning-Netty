8f94311271676273953808fbce99f01b3c78b4fc
pengzhe <pengzhe@rayvision.com>
2017-12-21 00:06:41 +0800

    Netty私有协议栈编解码

---
+++ src/main/java/com/javaman/netty/prototol/MarshallingCodecFactory.java
@@ -0,0 +1,44 @@
+package com.javaman.netty.prototol;
+
+import org.jboss.marshalling.*;
+
+import java.io.IOException;
+
+/**
+ * @author:彭哲
+ * @Date:2017/12/20
+ */
+public final class MarshallingCodecFactory {
+    /**
+     * 创建Jboss Marshaller
+     *
+     * @return
+     * @throws IOException
+     */
+    protected static Marshaller buildMarshalling() throws IOException {
+        final MarshallerFactory marshallerFactory
+                = Marshalling.getProvidedMarshallerFactory("serial");
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        Marshaller marshaller = marshallerFactory.createMarshaller(configuration);
+        return marshaller;
+    }
+
+    /**
+     * 创建Jboss Unmarshaller
+     *
+     * @return
+     * @throws IOException
+     */
+    protected static Unmarshaller buildUnmarshaller() throws IOException {
+        final MarshallerFactory marshallerFactory
+                = Marshalling.getProvidedMarshallerFactory("serial");
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        final Unmarshaller unmarshaller = marshallerFactory.createUnmarshaller(configuration);
+        return unmarshaller;
+
+    }
+
+
+}
